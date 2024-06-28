90f950077e41afe17d47adac58ad5af3d04e67c2
Julia Piaskowski <julia.piask@gmail.com>
2024-06-27 18:09:45 -0700

    update hero

--- assets/media/homespun/hero_generation.R
+++ assets/media/homespun/hero_generation.R
@@ -3,21 +3,26 @@ library(ggridges); library(readxl); library(lubridate);
 library(dplyr); library(ggplot2); library(tidyr)
 
 
-mydata2 <- read_excel("assets/media/homespun/environmental_data.xlsx") %>% 
+mydata2 <- read_excel("assets/media/homespun/environmental_data.xlsx") |>  
   mutate(month = month(DateTime, label = TRUE), 
          year = as.character(year(DateTime)), 
          week = as.factor(week(DateTime)))
 
 
-ggplot(mydata2, aes(x = nmpi_mn, y = week)) +
+ggplot(mydata2, aes(x = nmpi_mn, y = week, fill = week, color = week)) +
   geom_density_ridges(scale = 7, rel_min_height = .001,
-                      fill = "cyan", color = "transparent", alpha = 0.1) + 
+                      color = "transparent", show_guide = FALSE) + 
   geom_density_ridges(scale = 7, rel_min_height = 0.001, size = .3,
-                      fill = "transparent", color = "cyan") + 
+                      fill = "transparent", show_guide = FALSE) + 
+  scale_colour_viridis_d(option = "A", direction = 1) + 
+  scale_fill_viridis_d(option = "A", direction = 1, alpha = 0.8) +
   xlim(c(-10,75)) +
   theme_void() + 
   theme(panel.background = element_rect(fill = 'transparent', color = NA))
   
 
 ggsave("assets/media/homespun/hero.png", width = 4, height = 6)
+ggsave("content/home/hero.png", width = 4, height = 6)
+
+
 #goes in content/home
